350666657015132f45c7152be4806d8088550e70
NAVEENA M G <79916384+iamnaveenamg@users.noreply.github.com>
2026-07-03 00:58:36 +0530

    Create grader function and API integration in SQL
    This SQL script creates an API integration and an external function for grading, along with some select queries to validate the function and check schemas in the garden_plants database.

---
+++ DataWarehouse/create_grader_function.sql
@@ -0,0 +1,50 @@
+--dora01
+
+use role accountadmin;
+
+create or replace api integration dora_api_integration
+api_provider = aws_api_gateway
+api_aws_role_arn = 'arn:aws:iam::321463406630:role/snowflakeLearnerAssumedRole'
+enabled = true
+api_allowed_prefixes = ('https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora');
+
+
+use role accountadmin;  
+
+create or replace external function util_db.public.grader(
+      step varchar
+    , passed boolean
+    , actual integer
+    , expected integer
+    , description varchar)
+returns variant
+api_integration = dora_api_integration 
+context_headers = (current_timestamp, current_account, current_statement, current_account_name) 
+as 'https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora/grader'
+; 
+
+use role accountadmin;
+use database util_db; 
+use schema public; 
+
+select grader(step, (actual = expected), actual, expected, description) as graded_results from
+(SELECT 
+ 'DORA_IS_WORKING' as step
+ ,(select 123) as actual
+ ,123 as expected
+ ,'Dora is working!' as description
+); 
+
+select * 
+from garden_plants.information_schema.schemata;
+
+
+
+SELECT * 
+FROM GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA
+where schema_name in ('FLOWERS','FRUITS','VEGGIES'); 
+
+
+select count(*) as schemas_found, '3' as schemas_expected 
+from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA
+where schema_name in ('FLOWERS','FRUITS','VEGGIES'); 
